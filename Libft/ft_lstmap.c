fe7d6106720de245e6a09437973631b6ebee21a8
moist-bread <raquelgo2work@gmail.com>
2024-11-04 12:50:37 +0000

    normie

--- Libft/ft_lstmap.c
+++ Libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: rduro-pe <rduro-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 20:03:03 by rduro-pe          #+#    #+#             */
-/*   Updated: 2024/11/04 12:46:58 by rduro-pe         ###   ########.fr       */
+/*   Updated: 2024/11/04 12:49:45 by rduro-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list *new;
-	t_list *node;
-	
+	t_list	*new;
+	t_list	*node;
+
 	if (lst == NULL || f == NULL)
 		return (NULL);
 	new = NULL;
@@ -28,85 +28,10 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		if (node == NULL)
 		{
 			ft_lstclear(&new, del);
-			return(NULL);
+			return (NULL);
 		}
 		ft_lstadd_back(&new, node);
 		lst = lst->next;
 	}
-	return(new);
-}
-/*
-A FUNCAO f RETORNA CONTEUDO ITERADO, NAO ITERA DIRETAMENTE
-
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	t_list	*tmp;
-	t_list	*next;
-
-	tmp = *lst;
-	while (tmp)
-	{
-		del(tmp->content);
-		next = tmp->next;
-		free(tmp);
-		tmp = next;
-	}
-	*lst = NULL;
-}
-
-void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list	*last;
-
-	last = ft_lstlast(*lst);
-	if (last == NULL)
-		*lst = new;
-	else
-		last->next = new;
-}
-
-void	ft_lstiter(t_list *lst, void (*f)(void *))
-{
-	while (lst)
-	{
-		f(lst->content);
-		lst = lst->next;
-	}
-}
-t_list	*ft_lstnew(void *content)
-{
-	t_list	*new;
-
-	new = malloc(sizeof(t_list));
-	if (new == 0)
-		return (NULL);
-	new->content = content;
-	new->next = NULL;
 	return (new);
 }
-
-void	ft_striteri(char *s, void (*f)(unsigned int, char *))
-{
-	unsigned int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		f(i, &s[i]);
-		i++;
-	}
-}
-
-void	iter(unsigned int i, char *s)
-{
-	*s += i;
-}
-
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
-{
-	if (lst == NULL)
-		return (NULL);
-	del(lst->content);
-	free(lst);
-}
-*/
