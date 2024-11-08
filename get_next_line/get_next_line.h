d249e4d4185a58d1c1a984099d1a82311b133506
moist-bread <raquelgo2work@gmail.com>
2024-11-08 18:04:49 +0000

    get next line start

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rduro-pe <rduro-pe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 15:44:29 by rduro-pe          #+#    #+#             */
+/*   Updated: 2024/11/08 15:45:02 by rduro-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdarg.h>
+# include <stdio.h>
+# include <unistd.h>
+
+char *get_next_line(int fd);
+
+#endif
