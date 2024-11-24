2dc1963560306099bce69603697e08bf161b862b
moist-bread <raquelgo2work@gmail.com>
2024-11-24 15:10:22 +0000

    scriptprogress

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -9,26 +9,50 @@ ppro=$(grep "physical id" /proc/cpuinfo | wc -l)
 #virtual processors
 vpro=$(grep processor /proc/cpuinfo | wc -l)
 
-free --mega | awk '$1 == "Mem:" {print $3}' #ram used
-free --mega | awk '$1 == "Mem:" {print $2}' #ram total
-free --mega | awk '$1 == "Mem:" {printf("(%.2f%%)\n", ($3*100)/$2)}' #ram usage percentage
-df -m | grep "/dev/" | grep -v "/boot" | awk '{diskst_used += $3} END {print diskst_used}' #disk space used
-df -m | grep "/dev/" | grep -v "/boot" | awk '{diskst_total += $2} END {printf ("%.0fGb\n"), diskst_total/1024}' #disk space total
-df -m | grep "/dev/" | grep -v "/boot" | awk '{diskst_used += $3} {diskst_total += $2} END {printf("(%d%%)\n"), (diskst_used*100)/diskst_total}'
-vmstat 1 3 | tail -1 | awk '{print $15}' #cpu wait time
-who -b | awk '$1 == "system" {print $3 " " $4}' #last reboot date
-if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then #logical volume manager status
+#ram used
+ramu=$(free --mega | awk '$1 == "Mem:" {print $3}')
+
+#ram total
+ramt=$(free --mega | awk '$1 == "Mem:" {print $2}')
+
+#ram usage percentage
+ramp=$(free --mega | awk '$1 == "Mem:" {printf("(%.2f%%)\n", ($3*100)/$2)}')
+
+#disk space used
+disku=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{diskst_used += $3} END {print diskst_used}')
+
+#disk space total
+diskt=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{diskst_total += $2} END {printf ("%.0fGb\n"), diskst_total/1024}')
+
+diskp=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{diskst_used += $3} {diskst_total += $2} END {printf("(%d%%)\n"), (diskst_used*100)/diskst_total}')
+
+#cpu wait time
+cpuwt=$(vmstat 1 3 | tail -1 | awk '{print $15}')
+
+#last reboot date
+reboot=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+
+#logical volume manager status
+lvm=$( if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ]; then
 	echo yes
 else
 	echo no
-fi
-ss -at | grep ESTAB | wc -l #tcp connections
-users | wc -w #users
-ip link | grep "link/ether" | awk '{print $2}' #ipv4
-journalctl _COMM=sudo | grep COMMAND | wc -l #commands executed with sudo
+fi )
 
-wall "
-		#Architecture: $architecture
+#tcp connections
+tcp=$(ss -at | grep ESTAB | wc -l)
+
+#users
+users=$(users | wc -w)
+
+#ipv4
+ip=$(ip link | grep "link/ether" | awk '{print $2}')
+
+#commands executed with sudo
+sudo=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+
+#displayed message
+wall "	#Architecture: $architecture
 		#CPU physical: $ppro
 		#vCPU: $vpro
 		#Memory Usage:
@@ -39,5 +63,4 @@ wall "
 		#Connections TCP:
 		#User log:
 		#Network:
-		#Sudo:
-"
+		#Sudo:"
