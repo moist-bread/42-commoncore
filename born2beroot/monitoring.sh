baabc2b114c05f8fc2a45456aaa4f461118dbfc1
moist-bread <raquelgo2work@gmail.com>
2024-11-27 19:26:17 +0000

    script cringe

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -16,18 +16,21 @@ ramu=$(free --mega | awk '$1 == "Mem:" {print $3}')
 ramt=$(free --mega | awk '$1 == "Mem:" {print $2}')
 
 #ram usage percentage
-ramp=$(free --mega | awk '$1 == "Mem:" {printf("(%.2f%%)\n", ($3*100)/$2)}')
+ramp=$(free --mega | awk '$1 == "Mem:" {printf("%.2f", ($3*100)/$2)}')
 
 #disk space used
-disku=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{diskst_used += $3} END {print diskst_used}')
+disku=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} END {print disk_u}')
 
 #disk space total
 diskt=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{diskst_total += $2} END {printf ("%.0fGb\n"), diskst_total/1024}')
 
-diskp=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{diskst_used += $3} {diskst_total += $2} END {printf("(%d%%)\n"), (diskst_used*100)/diskst_total}')
+#disk space percentage
+diskp=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} {disk_t += $2} END {printf("%d"), (disk_u*100)/disk_t}')
 
 #cpu wait time
-cpuwt=$(vmstat 1 3 | tail -1 | awk '{print $15}')
+cpuid=$(vmstat 1 3 | tail -1 | awk '{print $15}')
+cpu_ex=$(expr 100 - $cpuid)
+cpuwt=$(printf "%.1f" $cpu_ex)
 
 #last reboot date
 reboot=$(who -b | awk '$1 == "system" {print $3 " " $4}')
@@ -46,21 +49,24 @@ tcp=$(ss -at | grep ESTAB | wc -l)
 users=$(users | wc -w)
 
 #ipv4
-ip=$(ip link | grep "link/ether" | awk '{print $2}')
+ip=$(hostname -i)
+mac=$(ip link | grep "link/ether" | awk '{print $2}')
 
 #commands executed with sudo
 sudo=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
 
 #displayed message
-wall "	#Architecture: $architecture
-		#CPU physical: $ppro
-		#vCPU: $vpro
-		#Memory Usage:
-		#Disk Usage:
-		#CPU load:
-		#Last boot:
-		#LVM use:
-		#Connections TCP:
-		#User log:
-		#Network:
-		#Sudo:"
+wall "
+	#Architecture: $architecture
+	#CPU physical: $ppro
+	#vCPU: $vpro
+	#Memory Usage: $ramu/$ramt MB ($ramp%)
+	#Disk Usage: $disku/$diskt ($diskp%)
+	#CPU load: $cpuwt%
+	#Last boot: $reboot
+	#LVM use: $lvm
+	#Connections TCP: $tcp ESTABLISHED
+	#User log: $users
+	#Network: IP $ip ($mac)
+	#Sudo: $sudo cmd"
+
