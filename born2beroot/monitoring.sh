2d4e9c5fc7c162ae0132a28f7792274332fe0179
moist-bread <raquelgo2work@gmail.com>
2024-11-29 21:55:22 +0000

    tested

--- born2beroot/monitoring.sh
+++ born2beroot/monitoring.sh
@@ -4,7 +4,7 @@
 architecture=$(uname -a)
 
 #physical processors
-ppro=$(grep "physical id" /proc/cpuinfo | wc -l)
+#ppro=$(grep "physical id" /proc/cpuinfo | wc -l)
 lscpu | awk '$1 == "CPU(s):" {print $2}'
 #try with --physical and experiment for the virtual
 
@@ -15,7 +15,7 @@ vpro=$(grep processor /proc/cpuinfo | wc -l)
 ramu=$(free --mega | awk '$1 == "Mem:" {printf("%i/%iMB (%.2f%%)", $3, $2, ($3*100)/$2)}')
 
 #disk space used
-disku=$(df -m  --total / | tail -1 | awk '$1 == "total" {print $3}')
+disku=$(df -m  --total / /home /dev/shm | tail -1 | awk '$1 == "total" {print $3}')
 #disku=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} END {print disk_u}')
 
 #disk space total
@@ -25,7 +25,7 @@ disku=$(df -m  --total / | tail -1 | awk '$1 == "total" {print $3}')
 #diskp=$(df -m | grep "/dev/" | grep -v "/boot" | awk '{disk_u += $3} {disk_t += $2} END {printf("%d"), (disk_u*100)/disk_t}')
 
 #disk space total and usage
-diskt=$(df -h  --total / | tail -1 | awk '$1 == "total" {printf("%sb (%s)", $2, $5)}')
+diskt=$(df -h  --total / /home /dev/shm | tail -1 | awk '$1 == "total" {printf("%sb (%s)", $2, $5)}')
 
 #cpu wait time
 #cpuid=$(vmstat 1 3 | tail -1 | awk '{print $15}')
