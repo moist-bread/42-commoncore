979ea977a1498ada2ddedf4bf9608702942e622f
moist-bread <raquelgo2work@gmail.com>
2024-12-13 18:18:55 +0000

    calculating your mum

--- push_swap/sorting.c
+++ push_swap/sorting.c
@@ -6,7 +6,7 @@
 /*   By: rduro-pe <rduro-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 23:36:27 by rduro-pe          #+#    #+#             */
-/*   Updated: 2024/12/12 15:31:37 by rduro-pe         ###   ########.fr       */
+/*   Updated: 2024/12/13 18:18:23 by rduro-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,11 @@ void	sort_stack(t_stacks *stk)
 	mover = malloc(sizeof(t_moves));
 	pb_do(stk);
 	pb_do(stk);
+	rb_do(stk);
+	pb_do(stk);
+	// pb_do(stk);
 	sort_calc(stk, mover);
+	execute_move(stk, mover);
 	free(mover);
 }
 
@@ -41,37 +45,75 @@ void	sort_calc(t_stacks *stk, t_moves *mover)
 	int id_b;
 	int dist_a;
 	int dist_b;
+	int rot;
 	
 	id_a = stk->atop_id;
 	id_b = stk->btop_id;
 	mover->bst_moves = 1;
-	if (stk->a[id_a] > stk->b[id_b] || stk->a[id_a] < stk->b[0])
+	if (stk->a[id_a] > stk->b[id_b] && stk->a[id_a] < stk->b[0]) // MAKE WORK FOR THE SAMLLEST NUMBER
 	{
 		mover->bst_id = id_a;
 		return;	
 	}
-	//mover->moves = 0;
-	while (--id_b >= 0)
-		if (stk->a[id_a] > stk->b[id_b] && stk->a[id_a] < stk->b[id_b + 1])
-			break;	 
-	dist_a = stk->atop_id / 2 - id_a;
-	dist_b = stk->btop_id / 2 - id_b;
-	if (dist_a < 0 && dist_b < 0)
+	while (id_a + 1)
 	{
-		if (dist_b < dist_a)
-			dist_a = dist_b;	
-		mover->cur_moves = -dist_a + 1;
+		rot = 0;
+		id_b = stk->btop_id + 1;		
+		while (--id_b >= 0)
+		{
+			if (id_b != stk->btop_id)
+			{	
+				if (stk->a[id_a] > stk->b[id_b] && stk->a[id_a] < stk->b[id_b + 1])
+					break;
+			}
+			else if	(stk->a[id_a] > stk->b[id_b] && stk->a[id_a] < stk->b[0])
+				break;
+		}
+		if (id_a >= ((stk->atop_id + 1) / 2) + ((stk->atop_id + 1) % 2))
+		{
+			dist_a = stk->atop_id - id_a;
+			rot++;
+		}
+		else
+		{
+			dist_a = id_a + 1;
+			rot--;	
+		}
+		if (id_b >= ((stk->btop_id + 1) / 2) + ((stk->btop_id + 1) % 2))
+		{
+			dist_b = stk->btop_id - id_b;
+			rot++;
+		}
+		else
+		{
+			dist_b = id_b + 1;
+			rot--;	
+		}
+		ft_printf("%d(id_a)\n", id_a);
+		ft_printf("%d(id_b)\n", id_b);
+		ft_printf("%d(dist_a)\n", dist_a);
+		ft_printf("%d(dist_b)\n", dist_b);
+		if (rot == 2 || rot == -2 || stk->btop_id == 1)
+		{
+			if (dist_b > dist_a)
+				dist_a = dist_b;	
+			mover->cur_moves = dist_a + 1;
+			ft_printf("rot %d\n", rot);
+		}
+		else
+			mover->cur_moves = dist_a + dist_b + 1;
+		if (id_a == stk->atop_id || mover->bst_moves > mover->cur_moves)
+		{
+			mover->bst_moves = mover->cur_moves;
+			mover->bst_id = id_a;
+		}
+		ft_printf("%d(cur move)\n", mover->cur_moves);
+		ft_printf("%d(bst move)\n", mover->bst_moves);
+		ft_printf("%d(bst id)\n\n", mover->bst_id);	
+		// if (mover->bst_moves == 2)
+		// 	break;
+		id_a--;	
 	}
-	if (dist_a >= 0 && dist_b >= 0)
-	{
-		if (dist_b > dist_a)
-			dist_a = dist_b;	
-		mover->cur_moves = dist_a + 1;
-	}
-	//	if(stk->atop_id / 2 - id_a)  = amount of ra
-	//	if((12 / 2 - 10 (= -4)) < 0); (12 / 2 - 10) * -1  = amount of ra; else = amount of rra; 		
-	//rev_sort_check(stk->b, stk->btop_id);
-	
 }
 
 void	self_sort(t_stacks *stks, int *stk, int top)
