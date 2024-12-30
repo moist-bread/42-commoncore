24bd517be4341e378c23793bca7ed3bf39ece589
moist-bread <raquelgo2work@gmail.com>
2024-12-30 14:53:02 +0000

    :3

--- push_swap/inc/checker.h
+++ push_swap/inc/checker.h
@@ -6,18 +6,17 @@
 /*   By: rduro-pe <rduro-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 14:38:59 by rduro-pe          #+#    #+#             */
-/*   Updated: 2024/12/30 14:40:17 by rduro-pe         ###   ########.fr       */
+/*   Updated: 2024/12/30 14:52:52 by rduro-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef CHECKER_H
 # define CHECKER_H
 
-#include "push_swap.h"
+# include "push_swap.h"
 
-int	exe_intructions(t_stacks *stk);
-int	command_assign(char *command, t_stacks *stk);
+int		exe_intructions(t_stacks *stk);
+int		command_assign(char *command, t_stacks *stk);
 void	check_ok(t_stacks *stk);
 
 #endif
