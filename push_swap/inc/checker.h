65935ae412a9afb965203154d5028136179aa89f
moist-bread <raquelgo2work@gmail.com>
2024-12-30 14:50:15 +0000

    checker header

---
+++ push_swap/inc/checker.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rduro-pe <rduro-pe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/30 14:38:59 by rduro-pe          #+#    #+#             */
+/*   Updated: 2024/12/30 14:40:17 by rduro-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef CHECKER_H
+# define CHECKER_H
+
+#include "push_swap.h"
+
+int	exe_intructions(t_stacks *stk);
+int	command_assign(char *command, t_stacks *stk);
+void	check_ok(t_stacks *stk);
+
+#endif
