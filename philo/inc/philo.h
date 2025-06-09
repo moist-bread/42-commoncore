c3457e867a70112e32b0954401d84559bfd57e9d
moist-bread <raquelgo2work@gmail.com>
2025-06-09 18:01:43 +0100

    Started philo

---
+++ philo/inc/philo.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rduro-pe <rduro-pe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 15:22:02 by rduro-pe          #+#    #+#             */
+/*   Updated: 2025/06/09 17:36:58 by rduro-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+// LIBS
+# include "libft/libft.h"
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+// DEFINES
+// colors
+# define YEL "\e[0;33m"
+# define BLU "\e[0;34m"
+# define CYN "\e[0;36m"
+# define DEF "\e[0m"
+
+// bg colors
+# define YELB "\e[43m"
+
+// TBD
+
+
+#endif
